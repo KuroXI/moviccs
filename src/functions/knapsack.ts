9bdbea752e8a585d30c6f4954e9f35e13ef3837a
Arvicss <ramosarvyx@gmail.com>
2024-03-25 19:15:58 +0800

    Update knapsack.ts

--- src/functions/knapsack.ts
+++ src/functions/knapsack.ts
@@ -8,7 +8,13 @@ type KnapsackInput = {
   orders: RowSelection[];
 };
 
-export const knapsack = ({ maxWeight, orders }: KnapsackInput) => {
+type knapsackOutput = {
+  orders: RowSelection[];
+  totalWeight: number;
+  ratio: number;
+};
+
+export const knapsackOriginal = ({ maxWeight, orders }: KnapsackInput) => {
   orders = selectionSort(orders, (a, b) => getPriceWeightRatio(a.order.items) - getPriceWeightRatio(b.order.items));
 
   let totalWeight = 0;
@@ -26,3 +32,55 @@ export const knapsack = ({ maxWeight, orders }: KnapsackInput) => {
 
   return sack;
 };
+
+export const knapsack = ({ maxWeight, orders } : KnapsackInput )  => {
+  // Limit the number of orders to 10
+  const limitedList = orders.slice(0, 10);
+
+  let currentKnapsack : knapsackOutput = {
+    orders: [],
+    totalWeight: 0,
+    ratio: 0,
+  }
+
+  const totalSubsets = Math.pow(2, limitedList.length);
+  // All possible subsets NOT USED!.
+  const subsets: knapsackOutput[] = [];
+
+  for (let i = 0; i < totalSubsets; i++) {
+    const subset: RowSelection[] = [];
+    let subsetWeight = 0;
+    let ratio = 0;
+    let temp = i;
+
+    for (const order of limitedList) {
+      if (temp % 2 === 1 && Boolean(order)) {
+        subset.push(order);
+        subsetWeight += calculateTotalWeight(order.order.items);
+        ratio += getPriceWeightRatio(order.order.items);
+      }
+
+      temp = Math.floor(temp / 2);
+    }
+
+    subsets.push({
+      orders: subset,
+      totalWeight: subsetWeight,
+      ratio: ratio,
+    });
+
+    if (subsetWeight <= maxWeight && ratio > currentKnapsack.ratio) {
+      currentKnapsack = {
+        orders: subset,
+        totalWeight: subsetWeight,
+        ratio: ratio,
+      };
+    }
+  }
+
+  console.log("Current Knapsack: ", currentKnapsack);
+  console.log("Subset Weight: ", currentKnapsack.totalWeight);
+  console.log("Subset Ratio: ", currentKnapsack.ratio);
+
+  return currentKnapsack.orders;
+}
